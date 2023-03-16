ac0df4e1715b10b9777aa0159ffdc08a6872ec24
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-03-16 16:19:27 +0300

    all task completed

--- Seminar2/task12.py
+++ Seminar2/task12.py
@@ -5,25 +5,17 @@
 
 
 
-x = abs(int(input('Введите первое натуральное число X от 1 до 1000 ')))
-y = abs(int(input('Введите второе натуральное число Y от 1 до 1000 ')))
-if x < 1 or x > 1000 or y < 1 or y > 1000:
-    print('Вы ввели число не из заданного диапазона!')
-else:
-    S = x + y
-    P = x * y
-    stop = 0
-    for i in range(1001):
-        if stop != 1:
-            for j in range(1001):
-                if stop != 1:
-                    if i * j == P and i + j == S:
-                        print(i, j)
-                        stop = 1
-            else:
-                j = 1001
-        else:
-            i = 1001
+from math import sqrt
+
+summa_numbers = int(input('Введите сумму двух чисел: '))
+composition_numbers = int(input('Введите произведение двух чисел: '))
+
+number_X = summa_numbers/2 + sqrt((summa_numbers/2)**2 - composition_numbers)
+number_Y = summa_numbers/2 - sqrt((summa_numbers/2)**2 - composition_numbers)
+
+print('Первое загаданное число = ' , round(number_X,2))
+print('Второе загаданное число = ' , round(number_Y,2))
+
 
 
 
