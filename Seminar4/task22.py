86b58f9bad7e460aaa7d1a45b9392a2cd6d163e1
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-03-22 15:37:46 +0300

    task22 completed

---
+++ Seminar4/task22.py
@@ -0,0 +1,28 @@
+# Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа. n - кол-во элементов первого множества. m - кол-во
+# элементов второго множества. Затем пользователь вводит сами элементы множеств.
+# 11 6
+# 2 4 6 8 10 12 10 8 6 4 2
+# 3 6 9 12 15 18
+# 6 12
+
+from random import randint
+
+number_elements_one = int(input('Введите количество элементов в первом наборе чисел: '))
+number_elements_two = int(input('Введите количество элементов во втором наборе чисел: '))
+
+set_numbers_one = [randint(1, 10) for i  in range(number_elements_one)]
+print(f' Первый набор чисел: {set_numbers_one} ')
+
+set_numbers_two = [randint(1, 10) for i  in range(number_elements_two)]
+print(f' Второй набор чисел: {set_numbers_two} ')
+
+set_numbers_all = set(set_numbers_one).intersection(set(set_numbers_two))
+
+list(set_numbers_all).sort
+
+print(set_numbers_all)
+
+
+
