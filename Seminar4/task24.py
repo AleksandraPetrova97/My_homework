b7b215abb723821ad5fb5aa54cad6aa25e819577
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-03-25 16:19:53 +0300

    Seminar5 completed

--- Seminar4/task24.py
+++ Seminar4/task24.py
@@ -30,6 +30,33 @@ for i in range(number_bushes + 2):
 print()
 print(f' Максимальная сумма собранных ягод за один раз: {summa_max} ')  
 
+# count_1=int(input("Введите количество элементов 1 набора = "))
+# list_1=list(int(input(f" Введите {i} элемент 1 набора = ")) for i in range(count_1))
+# count_2=int(input("Введите количество элементов 2 набора = "))
+# list_2=list(int(input(f" Введите {i} элемент 2 набора = ")) for i in range(count_2))
+# print(list_1)
+# print(list_2)
+# list_1=set(list_1)
+# list_2=set(list_2)
+# new_list=list_1.intersection(list_2)
+# print(f"Результат = {sorted(new_list)}")
+
+
+# count=int(input("Введите количество кустов = "))
+# list_new=list(int(input(f" Введите количество ягод {i} куста = ")) for i in range(count))
+# print(list_new)
+# sum_max=list_new[0]+list_new[1]+list_new[count-1]
+# number_max=0
+# for i in range(1,count):
+# if i!=count-1:
+# sum_temp=list_new[i]+list_new[i-1]+list_new[i+1]
+# else:
+# sum_temp=list_new[i]+list_new[i-1]+list_new[0]
+# if sum_temp>sum_max:
+# sum_max=sum_temp
+# number_max=i
+
+# print(f" Максимальная сумма ягод = {sum_max} c {number_max} куста")
 
 
 
