60941b2d5d7f9a089daec803a4c2c95ca981e1d0
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-04-03 08:16:31 +0300

    ok

--- Seminar6/task32.py
+++ Seminar6/task32.py
@@ -1,14 +1,14 @@
 # Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
-
 from random import randint
 
 def index_element(list_n, minimum, maximum):
-    for i in range(len(list_n)):
-        if minimum <= list_1[i] <= maximum:
-            print(i, end= " ")
-     
+    # for i in range(len(list_n)):
+        # if minimum <= list_1[i] <= maximum:
+        #     print(i, end= " ")
+    return [i for i, elem in enumerate(list_n) if minimum <= elem <= maximum]
+
 list_1 = [randint(1,10) for i in range(int(input('Введите количество элементов списка: ')))]
 
 print(list_1)
@@ -16,7 +16,10 @@ print(list_1)
 number_minimum = int(input('Введите минимальное число в диапазоне: '))
 number_maximum = int(input('Введите максимальное число в диапазоне: '))
 
-index_element(list_1, number_minimum, number_maximum)
+print(index_element(list_1, number_minimum, number_maximum))
+
+
+
 
 
 
